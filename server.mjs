757099f5e1923cf82cac2bcb8443ed1de0bdd8e7
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-11-16 22:26:06 +0100

    deploy better

---
+++ server.mjs
@@ -0,0 +1,34 @@
+import Koa from 'koa';
+import serve from 'koa-static';
+import send from 'koa-send';
+
+const port = process.env.PORT || 3000;
+const app = new Koa();
+
+/* force https */
+app.use(async (ctx, next) => {
+  if (process.env.NODE_ENV === 'production' && (ctx.request.headers['x-forwarded-proto'] !== 'https')) {
+    return ctx.redirect(ctx.request.href.replace('http://', 'https://'));
+  }
+  return await next();
+})
+
+/* serve production build files */
+app.use(serve('build'));
+
+/* serve index.html for all 404 routes */
+app.use(async (ctx) => {
+  await send(ctx, 'build/index.html');
+})
+
+async function server() {
+  /* Finally, start the server */
+  await app.listen(port);
+  /* Yay */
+  console.log('Server listening on port', port);
+}
+
+server().catch((err)=> {
+  console.error(err);
+  process.exit(1);
+});
--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const STACK_NAME = process.env.VUE_APP_STACK_NAME
 
 export const CLIENT_URL = process.env.NODE_ENV === 'production'
-  ? 'https://' + STACK_NAME + '.tokenbb.io'
+  ? 'https://' + STACK_NAME + '.tokenbb.bt-stage.com'
   : 'http://localhost:8080'
 
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? CLIENT_URL + '/api/v1'
+  ? 'https://api.bt-stage.com/v1/forum/monsters'
   : 'http://localhost:3030/v1/forum/monsters'
 
 export const CONNECT_API_URL = 'https://steemconnect.com'
