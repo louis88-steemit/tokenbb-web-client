1300fa6e01dd960a74f56b15cf226bc357167167
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2018-12-07 10:39:47 +0100

    update all deps

--- server.mjs
+++ server.mjs
@@ -8,37 +8,39 @@ const port = process.env.PORT || 3000;
 const app = new Koa();
 
 /* force https */
-app.use(async (ctx, next) => {
-  if (process.env.NODE_ENV === 'production' && (ctx.request.headers['x-forwarded-proto'] !== 'https')) {
-    return ctx.redirect(ctx.request.href.replace('http://', 'https://'));
+app.use( async ( ctx, next ) => {
+  if ( process.env.NODE_ENV === 'production' && ( ctx.request.headers['x-forwarded-proto'] !== 'https' ) ) {
+    return ctx.redirect( ctx.request.href.replace( 'http://', 'https://' ) );
   }
   return await next();
-});
+} );
 
-app.use(serve('dist/service-worker.js'))
-app.use(noCache({
-  paths: ['dist/service-worker.js'],
-}))
+app.use( serve( 'dist/service-worker.js' ) );
+app.use( noCache( {
+  paths: [ 'dist/service-worker.js' ],
+} ) );
 
-const oneWeek       = 1000 * 60 * 60 * 24 * 7;
+const oneWeek = 1000 * 60 * 60 * 24 * 7;
 
-app.use(serveCache('dist', {
-  maxAge: 365 * 24 * 60 * 60
-}))
+app.use( serveCache( 'dist', {
+  maxAge: 365 * 24 * 60 * 60,
+} ) );
 
 /* serve index.html for all 404 routes */
-app.use(async (ctx) => {
-  await send(ctx, 'dist/index.html');
-});
+app.use( async ( ctx ) => {
+  await send( ctx, 'dist/index.html' );
+} );
 
 async function server() {
+
   /* Finally, start the server */
-  await app.listen(port);
+  await app.listen( port );
+
   /* Yay */
-  console.log('Server listening on port', port);
+  console.log( 'Server listening on port', port );
 }
 
-server().catch((err)=> {
-  console.error(err);
-  process.exit(1);
-});
+server().catch( ( err ) => {
+  console.error( err );
+  process.exit( 1 );
+} );
