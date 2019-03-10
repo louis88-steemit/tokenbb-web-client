a81b30a0c1ff9847d934fcc512a22d6c672ed5f0
Evan Chou <chou86.e@gmail.com>
2019-03-10 19:08:29 +0000

    fix warning, use value instead of store directly

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -128,7 +128,7 @@ export default {
     categoryList( value ) {
       const queryCategory = this.$route.query.category;
       if ( this.$route.query.category && !this.selectedCategoryId ) {
-        const selectedCategory = this.$store.state.categories.categoryList.find( ( category ) => {
+        const selectedCategory = value.find( ( category ) => {
           return category.slug === queryCategory
             || category._id === queryCategory;
         } ) || {};
