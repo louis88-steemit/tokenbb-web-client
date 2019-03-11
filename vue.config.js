fd09ca205a8dc2db2ab92f9ad957cc7e419cd9e3
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2019-03-11 14:44:56 +0100

    try compression

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,31 @@
+const BrotliPlugin = require( 'brotli-webpack-plugin' );
+const CompressionPlugin = require( 'compression-webpack-plugin' );
+const zopfli = require( '@gfx/zopfli' );
 
 module.exports = {
   pwa: {
     workboxOptions: {
       importScripts: [ 'skip-waiting.js' ],
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new BrotliPlugin( {
+        asset: '[path].br[query]',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 0,
+        minRatio: 0.8,
+      } ),
+      new CompressionPlugin( {
+        compressionOptions: {
+          numiterations: 15,
+        },
+        test: /\.(js|css|html|svg)$/,
+        filename: '[path].gz[query]',
+        algorithm( input, compressionOptions, callback ) {
+          return zopfli.gzip( input, compressionOptions, callback );
+        },
+      } ),
+    ],
   },
 };
