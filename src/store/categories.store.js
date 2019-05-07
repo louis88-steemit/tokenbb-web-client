c1fad49aff410a55a4ef2c1a9b0a472184697bc5
Evan Chou <chou86.e@gmail.com>
2019-05-07 03:46:36 +0000

    changes for category order edit / add cats

--- src/store/categories.store.js
+++ src/store/categories.store.js
@@ -5,6 +5,88 @@ import { errorAlertOptions } from '../utils/notifications.js';
 
 import map from 'lodash/map';
 
+function stringToSlug( inputStr ) {
+  let str = String( inputStr );
+  str = str.trim();
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = 'åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  const to = 'aaaaaaeeeeiiiioooouuuunc------';
+
+  for ( let i = 0, l = from.length; i < l; i++ ) {
+    str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
+  }
+
+  return str
+    .replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
+    .replace( /\s+/g, '-' ) // collapse whitespace and replace by -
+    .replace( /-+/g, '-' ) // collapse dashes
+    .replace( /^-+/, '' ) // trim - from start of text
+    .replace( /-+$/, '' ) // trim - from end of text
+    .substring( 0, 24 );
+}
+
+function computeCategoryOrderingData( state, categoryOrdering ) {
+  const categoriesBySlug = {};
+  state.categoryList.forEach( ( category ) => {
+    state.categoriesById[category._id] = category;
+    categoriesBySlug[category.slug] = category;
+  } );
+
+  const categoryGroupsByNav = {};
+  function processCategoryOrdering( ordering, nav = '' ) {
+    const slug = stringToSlug( ordering.slug );
+    const currentNav = nav + ( nav !== '' ? '/' : '' ) + slug;
+    const categoryGroup = { name: ordering.name, nav: currentNav, slug: ordering.slug };
+    categoryGroupsByNav[currentNav] = categoryGroup;
+    categoryGroup.groups = map( ordering.groups, ( g ) => {
+      return processCategoryOrdering( g, currentNav );
+    } );
+    categoryGroup.categories = map( ordering.categories, ( c ) => {
+      const found = categoriesBySlug[c];
+      if ( found ) {
+        found.nav = currentNav;
+        categoriesBySlug[c] = null;
+      }
+      return found;
+    } ).filter( ( c ) => c );
+    return categoryGroup;
+  }
+
+  let homeCategory = {
+    name: 'Home',
+    nav: 'home',
+    slug: 'home',
+    groups: [],
+    categories: [],
+  };
+
+  // when getter not ready, categoryOrdering is a function
+  if ( categoryOrdering && typeof categoryOrdering === 'object' ) {
+    if ( Array.isArray( categoryOrdering ) ) {
+      homeCategory.groups = categoryOrdering;
+      homeCategory = processCategoryOrdering( homeCategory );
+    } else {
+      homeCategory = processCategoryOrdering( categoryOrdering );
+    }
+  }
+
+  // Place other categories on root level.
+  homeCategory.categories = Object.values( categoriesBySlug )
+    .filter( ( c ) => c )
+    .map( ( c ) => {
+      c.nav = 'home';
+      return c;
+    } );
+  homeCategory.categoryGroupsByNav = categoryGroupsByNav;
+
+  state.categoriesByBreadcrumb = homeCategory;
+
+  // For Vue Reactivity.
+  state.categoriesById = { ...state.categoriesById };
+}
+
 export default {
   namespaced: true,
   state: {
@@ -27,94 +109,18 @@ export default {
     },
     updateCategoryList( state, categories ) {
       state.categoryList = categories;
-      const categoriesBySlug = {};
-      categories.forEach( ( category ) => {
-        state.categoriesById[category._id] = category;
-        categoriesBySlug[category.slug] = category;
-      } );
 
-      const categoryOrdering = this.getters['forum/getCategoryOrdering'];
-
-      function stringToSlug( inputStr ) {
-        let str = String( inputStr );
-        str = str.trim();
-        str = str.toLowerCase();
-
-        // remove accents, swap ñ for n, etc
-        const from = 'åàáãäâèéëêìíïîòóöôùúüûñç·/_,:;';
-        const to = 'aaaaaaeeeeiiiioooouuuunc------';
-
-        for ( let i = 0, l = from.length; i < l; i++ ) {
-          str = str.replace( new RegExp( from.charAt( i ), 'g' ), to.charAt( i ) );
-        }
-
-        return str
-          .replace( /[^a-z0-9 -]/g, '' ) // remove invalid chars
-          .replace( /\s+/g, '-' ) // collapse whitespace and replace by -
-          .replace( /-+/g, '-' ) // collapse dashes
-          .replace( /^-+/, '' ) // trim - from start of text
-          .replace( /-+$/, '' ) // trim - from end of text
-          .substring( 0, 24 );
-      }
-
-      const categoryGroupsByNav = {};
-      function processCategoryOrdering( ordering, nav = '' ) {
-        const slug = stringToSlug( ordering.slug );
-        const currentNav = nav + ( nav !== '' ? '/' : '' ) + slug;
-        const categoryGroup = { name: ordering.name, nav: currentNav };
-        categoryGroupsByNav[currentNav] = categoryGroup;
-        categoryGroup.groups = map( ordering.groups, ( g ) => {
-          return processCategoryOrdering( g, currentNav );
-        } );
-        categoryGroup.categories = map( ordering.categories, ( c ) => {
-          const found = categoriesBySlug[c];
-          if ( found ) {
-            found.nav = currentNav;
-            categoriesBySlug[c] = null;
-          }
-          return found;
-        } ).filter( ( c ) => c );
-        return categoryGroup;
-      }
-
-      let homeCategory = {
-        name: 'Home',
-        nav: 'home',
-        slug: 'home',
-        groups: [],
-        categories: [],
-      };
-
-      // when getter not ready, categoryOrdering is a function
-      if ( categoryOrdering && typeof categoryOrdering === 'object' ) {
-        if ( Array.isArray( categoryOrdering ) ) {
-          homeCategory.groups = categoryOrdering;
-          homeCategory = processCategoryOrdering( homeCategory );
-        } else {
-          homeCategory = processCategoryOrdering( categoryOrdering );
-        }
-      }
-
-      // Place other categories on root level.
-      homeCategory.categories = Object.values( categoriesBySlug )
-        .filter( ( c ) => c )
-        .map( ( c ) => {
-          c.nav = 'home';
-          return c;
-        } );
-      homeCategory.categoryGroupsByNav = categoryGroupsByNav;
-
-      state.categoriesByBreadcrumb = homeCategory;
-
-      // For Vue Reactivity.
-      state.categoriesById = { ...state.categoriesById };
+      computeCategoryOrderingData( state, this.getters['forum/getCategoryOrdering'] );
+    },
+    updateCategoryOrderingData( state ) {
+      computeCategoryOrderingData( state, this.getters['forum/getCategoryOrdering'] );
     },
   },
   actions: {
-    add( { commit }, { name: categoryName, title, description } ) {
+    add( { commit }, { name: categoryName, title, description, nav } ) {
       commit( 'setFetching', true );
 
-      addCategory( categoryName, title, description )
+      addCategory( categoryName, title, description, nav )
         .then( () => {
           commit( 'setFetching', false );
         } )
